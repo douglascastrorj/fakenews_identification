e8cd38942971c5d4d1e987f46e8014bb2ccea055
Douglas <douglascastrorj@gmail.com>
2019-05-19 18:30:16 -0300

    usando outras metricas e extracao de features pelo spacy

--- baseline.py
+++ baseline.py
@@ -14,88 +14,100 @@ from sklearn.datasets import fetch_20newsgroups
 import read_dataset as rd
 from text_processor import Preprocessor
 
-for i in range(0,10):
+## LENDO DATASET        ######################
+train,test = rd.read(percent_train=.7)
+categories = ['fake', 'real']
 
-  print('Treino: #',i + 1,'\n')
+train_text = rd.get_text(train)
 
-  ## LENDO DATASET        ######################
-  train,test = rd.read(percent_train=.7)
-  categories = ['fake', 'real']
+train_target = rd.get_target(train)
 
-  train_text = rd.get_text(train)
+test_text = rd.get_text(test)
 
-  train_target = rd.get_target(train)
+test_target = rd.get_target(test)
+#################################################
 
-  test_text = rd.get_text(test)
+### PROCESSANDO TEXTO ######
 
-  test_target = rd.get_target(test)
-  #################################################
+print('Processando texto...')
+n_gram = 0
+pos=False
+tags=False
+dep=False
+stem = False
+remove_stop_words = False
+remove_punct = False
 
-  ### PROCESSANDO TEXTO ######
-
-  print('Processando texto...')
-  n_gram = 3
-  tags=False
-  stem = True
-  remove_stop_words = True
-  remove_ponctuation = False
-
-  processor = Preprocessor()
-  train_text =  processor.process_dataset(
-                  train_text, 
-                  n_gram=n_gram, 
-                  stem=stem, 
-                  tags=tags,
-                  remove_stop_words=remove_stop_words, 
-                  remove_ponctuation=remove_ponctuation
-                )
-
-  test_text = processor.process_dataset(
-                test_text,
+processor = Preprocessor()
+train_text =  processor.process_dataset(
+                train_text, 
                 n_gram=n_gram, 
-                stem=stem,
+                stem=stem, 
                 tags=tags,
                 remove_stop_words=remove_stop_words, 
-                remove_ponctuation=remove_ponctuation
+                remove_punct=remove_punct,
+                pos=pos,
+                dep=dep
               )
 
-  ##              TREINANDO NAIVE               ##
+test_text = processor.process_dataset(
+              test_text,
+              n_gram=n_gram, 
+              stem=stem,
+              tags=tags,
+              remove_stop_words=remove_stop_words, 
+              remove_punct=remove_punct,
+              pos=pos,
+              dep=dep
+            )
 
-  print ('Treinando modelo...')
-  text_clf = Pipeline([
-                      ('vect', CountVectorizer()),
-                        ('tfidf', TfidfTransformer()),
-                        # ('clf', MultinomialNB()),
-                        ('clf', SGDClassifier(loss='hinge', penalty='l2',
-                                              alpha=1e-3, random_state=42,
-                                              max_iter=7, tol=None)),
-  ])
-  text_clf.fit(train_text , train_target)
-  print( 'Treino concluido.')
+##              TREINANDO NAIVE               ##
 
-  #avaliacao de desempenho no conjunto de teste
-  predicted = text_clf.predict(test_text)
-  print('acuracia ', np.mean(predicted == test_target) )
+print ('Treinando modelo...')
+text_clf = Pipeline([
+                    ('vect', CountVectorizer()),
+                      ('tfidf', TfidfTransformer()),
+                      ('clf', MultinomialNB()),
+                      # ('clf', SGDClassifier(loss='hinge', penalty='l2',
+                      #                       alpha=1e-3, random_state=42,
+                      #                       max_iter=7, tol=None)),
+])
+text_clf.fit(train_text , train_target)
+print( 'Treino concluido.')
 
+#avaliacao de desempenho no conjunto de teste
+predicted = text_clf.predict(test_text)
+print('acuracia ', np.mean(predicted == test_target) )
 
-  #metricas
-  print(metrics.classification_report(test_target, predicted,target_names=categories))
-  print(metrics.confusion_matrix(test_target, predicted))
+# Compute the precision
+# The precision is the ratio tp / (tp + fp) where tp is the number of true positives and fp the number of false positives. 
+# The precision is intuitively the ability of the classifier not to label as positive a sample that is negative.
 
-  print('Escrevendo arquivo de log\n')
-  file = open('log.txt', 'a')
+# Compute the recall
+# The recall is the ratio tp / (tp + fn) where tp is the number of true positives and fn the number of false negatives. 
+# The recall is intuitively the ability of the classifier to find all the positive samples.
+# The best value is 1 and the worst value is 0.
+#metricas
+print(metrics.classification_report(test_target, predicted,target_names=categories))
+print(metrics.f1_score(test_target, predicted))
+print(metrics.precision_score(test_target, predicted))
+print(metrics.recall_score(test_target, predicted))
+print(metrics.confusion_matrix(test_target, predicted))
 
-  file.write('Features utilizadas: \n' )
-  file.write('NGRAM: '+ str(n_gram) + '\n' )
-  file.write('tags: '+ str(tags) + '\n' )
-  file.write('stem: '+ str(stem) + '\n' )
-  file.write('Remove stopwords: '+ str(remove_stop_words) + '\n' )
-  file.write('Remove ponctuation: '+ str(remove_ponctuation) + '\n' )
-  file.write('\nAcuracia: ' + str(np.mean(predicted == test_target)) )
-  file.write('\n')
-  file.write(metrics.classification_report(test_target, predicted,target_names=categories))
-  file.write('\n')
-  file.write('Matriz de Confusao: \n\n')
-  file.write(np.array2string(metrics.confusion_matrix(test_target, predicted), separator=', '))
-  file.write('\n\n#############################################\n\n')
-  file.close() 
+# print('Escrevendo arquivo de log\n')
+# file = open('log.txt', 'a')
+
+# file.write('Features utilizadas: \n' )
+# file.write('NGRAM: '+ str(n_gram) + '\n' )
+# file.write('tags: '+ str(tags) + '\n' )
+# file.write('stem: '+ str(stem) + '\n' )
+# file.write('Remove stopwords: '+ str(remove_stop_words) + '\n' )
+# file.write('Remove ponctuation: '+ str(remove_punct) + '\n' )
+# file.write('\nAcuracia: ' + str(np.mean(predicted == test_target)) )
+# file.write('\n')
+# file.write(metrics.classification_report(test_target, predicted,target_names=categories))
+# file.write('\n')
+# file.write('Matriz de Confusao: \n\n')
+# file.write(np.array2string(metrics.confusion_matrix(test_target, predicted), separator=', '))
+# file.write('\n\n#############################################\n\n')
+# file.close() 
--- text_processor.py
+++ text_processor.py
@@ -5,19 +5,30 @@ from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 from nltk.tag import pos_tag_sents, pos_tag
-
+import spacy
 
 class Preprocessor():
     def __init__(self):
         self.stop_words = set(stopwords.words('english'))
         self.stemmer = PorterStemmer()
+        self.nlp = spacy.load('en_core_web_sm')
         pass
 
-    def process_dataset(self,dataset, remove_stop_words=False, stem=False, remove_ponctuation=False, n_gram=1, tags=False):
+    def process_dataset(    self,
+                            dataset, 
+                            remove_stop_words=False, 
+                            stem=False, 
+                            remove_punct=False, 
+                            n_gram=1, 
+                            tags=False, 
+                            pos=False, 
+                            dep=False, 
+                            alpha=False
+        ):
         return [ 
             self.proccess_text(
                                 text,
-                                remove_ponctuation=remove_ponctuation,
+                                remove_punct=remove_punct,
                                 remove_stop_words=remove_stop_words,
                                 stem=stem,
                                 n_gram=n_gram,
@@ -26,24 +37,46 @@ class Preprocessor():
             for text in dataset
         ]
     
-    def proccess_text(self, text, remove_stop_words=False, stem=False, remove_ponctuation=False, n_gram=1, tags=False):
-        words = word_tokenize(text)
-        if remove_stop_words :
-            words = [w for w in words if not w in self.stop_words]
-        if stem:
-            words = [ self.stemmer.stem(word) for word in words ]
+    def proccess_text(  self, 
+                        text, 
+                        remove_stop_words=False, 
+                        stem=False, 
+                        remove_punct=False, 
+                        n_gram=1, 
+                        tags=False, 
+                        pos=False, 
+                        dep=False, 
+                        alpha=False
+        ):
 
-        processed_text = ' '.join(words)
-
-        if tags:
-            try:
-                processed_text = self.apply_pos_tag(processed_text)
-            except:
-                print 'Error on apply pos tag '
+        features = []
+        n_grams = []
+        tokens = self.nlp(text)
+        if remove_punct:
+            tokens = [ token for token in tokens if token.dep_ != 'punct']
+        if remove_stop_words:
+            tokens = [token for token in tokens if token.is_stop == False]
         if n_gram > 1:
-            processed_text = self.n_gram(processed_text, n_gram)
+            n_grams = self.n_gram(tokens, n_gram)
+            
+        for token in tokens:
+            features.append(token.text)
 
-        return processed_text
+        for token in tokens:
+            if stem:
+                features.append('@feature_lemma_'+token.lemma_)
+            if pos:
+                features.append('@feature_pos_'+token.pos_)
+            if tags:
+                features.append( '@feature_tag_'+token.tag_)
+            if dep:
+                features.append('@feature_dep_'+ token.dep_)
+            if alpha:
+                # features.append('@feature_shape_'+token.shape_)
+                features.append('@feature_alpha_'+str(token.is_alpha))
+                # features.append('@feature_stop_'+ str(token.is_stop))
+        features = features + n_grams
+        return ' '.join(features)
     
     def tokenize(self, text):
         return word_tokenize(text)
@@ -60,25 +93,24 @@ class Preprocessor():
         result = words + gramatical_class
         return ' '.join(result)
 
-    def n_gram(self, text, n=2):
-        words = text.split(' ')
-        model = []
-        for i in range(0, len(words) - 1):
-            model.append(words[i])
-            gram = '@featureNGram'
+    def n_gram(self, tokens, n=2):
+        n_grams = []
+        for i in range(0, len(tokens) - 1):
+            n_grams.append(tokens[i].text)
+            gram = '@feature_NGram'
             for j in range(0, n):
-                if(i + j < len(words)):
-                    gram = gram + '_' + words[i+j]
-            model.append(gram)
-        model.append(words[len(words) - 1])
-        return ' '.join(model)
+                if(i + j < len(tokens)):
+                    gram = gram + '_' + tokens[i+j].text
+            n_grams.append(gram)
+        n_grams.append(tokens[len(tokens) - 1].text)
+        return n_grams
 
 # data = "All work and no play makes jack dull boy. All work and no play makes jack a dull boy."
 
 # data = 'hello my old friend, are you working so much?'
 # preprocessor = Preprocessor()
 # print preprocessor.n_gram(data, n=2)
-# print preprocessor.proccess_text(data, n_gram=2, tags=True, stem=True)
+# print( preprocessor.proccess_text(data, n_gram=2, tags=True, stem=True, remove_punct=True))
 # print preprocessor.apply_pos_tag(data)
 
 # dataset = [
